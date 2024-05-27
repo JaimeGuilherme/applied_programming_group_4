141448ab249962b89151e22756d84b1f161e397f
JaimeGuilherme <jaime.guilherme67@gmail.com>
2024-05-27 04:05:17 -0300

    updatedolucaocomplementar
    Input da solução complementar

--- algorithms/Projeto3/solucao_complementar.py
+++ algorithms/Projeto3/solucao_complementar.py
@@ -1,38 +1,33 @@
 from qgis.PyQt.QtCore import QCoreApplication
-from qgis.core import (QgsProcessing, QgsFeatureSink,
-                    QgsProcessingAlgorithm, QgsProcessingParameterFeatureSource,
-                    QgsProcessingParameterFeatureSink,
-                    QgsProcessingParameterRasterLayer, QgsFields, QgsWkbTypes, QgsVectorLayer, QgsFeature, QgsProject)
-from qgis import processing
-class Projeto2Solucao(QgsProcessingAlgorithm):
-    # Definição dos identificadores dos parâmetros e saídas
-    INPUT_CURVA = 'INPUT_CURVA'
-    INPUT_PISTA_PONTO  = 'INPUT_PISTA_PONTO'
-    INPUT_PISTA_LINHA = 'INPUT_PISTA_LINHA'
-    INPUT_PISTA_AREA = 'INPUT_PISTA_AREA'
-    INPUT_COMBOBOX= 'INPUT_COMBOBOX'
-    INPUT_MOLDURA = 'INPUT_MOLDURA'
-    INPUT_MDT = 'INPUT_MDT'
-    INPUT_ESCALA ='INPUT_ESCALA'
-    INPUT_MASCARA = 'INPUT_MASCARA'
+from qgis.core import (QgsProcessing, QgsFeatureSink, QgsProcessingException,
+                       QgsProcessingAlgorithm, QgsProcessingParameterFeatureSource,
+                       QgsProcessingParameterFeatureSink, QgsProcessingParameterDistance,
+                       QgsProcessingParameterField, QgsProcessingParameterEnum,
+                       QgsFeature, QgsGeometry, QgsPointXY, QgsWkbTypes, QgsApplication)
 
-    OUTPUT_CURVA = 'OUTPUT_DRENAGEM'
-    OUTPUT_PISTA_PONTO = 'OUTPUT_PISTA_PONTO'
-    OUTPUT_PISTA_LINHA = 'OUTPUT_PISTA_LINHA'
-    OUTPUT_PISTA_AREA = 'OUTPUT_PISTA_AREA'
-    OUTPUT_PONTO_COTA = 'OUTPUT_PONTO_COTA'
+class Projeto3Solucao(QgsProcessingAlgorithm):
+    INPUT_PONTOS = 'INPUT_PONTOS'
+    INPUT_DIA_1 = 'INPUT_DIA_1'
+    INPUT_DIA_2 = 'INPUT_DIA_2'
+    INPUT_TOL = 'INPUT_TOL'
+    INPUT_PRIMARY_KEY = 'INPUT_PRIMARY_KEY'
+    INPUT_IGNORA = 'INPUT_IGNORA'
+    OUTPUT_CURVA = 'OUTPUT_CURVA'
+    OUTPUT_MODIFICADOS = 'OUTPUT_MODIFICADOS'
+    OUTPUT_BUFFER = 'OUTPUT_BUFFER'
+    OUTPUT_MODIFICADOS_FORA = 'OUTPUT_MODIFICADOS_FORA'
 
     def tr(self, string):
-        return QCoreApplication.translate('Projeto2Solucao', string)
+        return QCoreApplication.translate('Projeto3Solucao', string)
 
     def createInstance(self):
-        return Projeto2Solucao()
+        return Projeto3Solucao()
 
     def name(self):
-        return 'projeto2solucao'
+        return 'projeto3solucao'
 
     def displayName(self):
-        return self.tr('Solução Projeto 2 - Grupo 4')
+        return self.tr('Solução Projeto 3 - Grupo 4')
 
     def group(self):
         return self.tr('Exemplos')
@@ -41,106 +36,72 @@ class Projeto2Solucao(QgsProcessingAlgorithm):
         return 'exemplos'
 
     def shortHelpString(self):
-        return self.tr("Este algoritmo é parte da solução do Grupo 4 para identificação da curva mestra.")
+        return self.tr("Identifica e extrai geometrias modificadas fora do buffer.")
 
     def initAlgorithm(self, config=None):
-        self.addParameter(QgsProcessingParameterFeatureSource(self.INPUT_CURVA, self.tr('Curva de Nível'), [QgsProcessing.TypeVectorLine]))
-        self.addParameter(QgsProcessingParameterFeatureSource(self.INPUT_MASCARA, self.tr("Camada de Moldura "), [QgsProcessing.TypeVectorPolygon]))
-        self.addParameter(QgsProcessingParameterRasterLayer(self.INPUT_MOLDURA, self.tr("Camada MDT")))
-
-       # Outputs
-        self.addParameter(QgsProcessingParameterFeatureSink(self.OUTPUT_PONTO_COTA, self.tr('Saída Cota'),QgsProcessing.TypeVectorPoint))
+        self.addParameter(QgsProcessingParameterFeatureSource(self.INPUT_PONTOS, self.tr('Camada de Pontos'), [QgsProcessing.TypeVectorPoint]))
+        self.addParameter(QgsProcessingParameterFeatureSource(self.INPUT_DIA_1, self.tr('Camada do Dia 1'), [QgsProcessing.TypeVectorPoint, QgsProcessing.TypeVectorLine, QgsProcessing.TypeVectorPolygon]))
+        self.addParameter(QgsProcessingParameterFeatureSource(self.INPUT_DIA_2, self.tr('Camada do Dia 2'), [QgsProcessing.TypeVectorPoint, QgsProcessing.TypeVectorLine, QgsProcessing.TypeVectorPolygon]))
+        self.addParameter(QgsProcessingParameterDistance(self.INPUT_TOL, self.tr('Distância de Tolerância'), defaultValue=10))
+        self.addParameter(QgsProcessingParameterField(self.INPUT_PRIMARY_KEY, self.tr('Chave Primária'), None, self.INPUT_DIA_1))
+        # self.addParameter(QgsProcessingParameterEnum(self.INPUT_IGNORA, self.tr('Atributos a ignorar'), [], allowMultiple=True, optional=True))
+        self.addParameter(QgsProcessingParameterFeatureSink(self.OUTPUT_CURVA, self.tr('Saída de Rota Percorrida'), QgsProcessing.TypeVectorLine))
+        self.addParameter(QgsProcessingParameterFeatureSink(self.OUTPUT_MODIFICADOS, self.tr('Geometrias Modificadas'), QgsProcessing.TypeVectorAnyGeometry))
+        self.addParameter(QgsProcessingParameterFeatureSink(self.OUTPUT_BUFFER, self.tr('Buffer da Rota'), QgsProcessing.TypeVectorPolygon))
+        self.addParameter(QgsProcessingParameterFeatureSink(self.OUTPUT_MODIFICADOS_FORA, self.tr('Geometrias Modificadas Fora do Buffer'), QgsProcessing.TypeVectorAnyGeometry))
 
     def processAlgorithm(self, parameters, context, feedback):
-        # Obter as camadas e parâmetros de entrada
-        curvaNivel = self.parameterAsVectorLayer(parameters, self.INPUT_CURVA, context)
-        raster = self.parameterAsRasterLayer(parameters, self.INPUT_MOLDURA, context)
-        moldura = self.parameterAsVectorLayer(parameters, self.INPUT_MASCARA, context)
+        pontos_layer = self.parameterAsVectorLayer(parameters, self.INPUT_PONTOS, context)
+        dia1_layer = self.parameterAsVectorLayer(parameters, self.INPUT_DIA_1, context)
+        dia2_layer = self.parameterAsVectorLayer(parameters, self.INPUT_DIA_2, context)
+        tol = self.parameterAsDouble(parameters, self.INPUT_TOL, context)
+        primary_key = self.parameterAsString(parameters, self.INPUT_PRIMARY_KEY, context)
+        ignora_indexes = self.parameterAsEnums(parameters, self.INPUT_IGNORA, context)
 
-        # Processamento: Criar campo altitude 
+        pontos_features = sorted(pontos_layer.getFeatures(), key=lambda feat: feat['creation_time'])
+        line_geometry = QgsGeometry.fromPolylineXY([QgsPointXY(feat.geometry().asPoint()) for feat in pontos_features])
+        (line_sink, line_dest_id) = self.parameterAsSink(parameters, self.OUTPUT_CURVA, context, pontos_layer.fields(), QgsWkbTypes.LineString, pontos_layer.sourceCrs())
+        line_feature = QgsFeature()
+        line_feature.setGeometry(line_geometry)
+        line_sink.addFeature(line_feature, QgsFeatureSink.FastInsert)
 
-        
-        # Filtrar curvas de nível dentro da área recortada do MDT
-        feedback.pushInfo('Filtrando curvas de nível dentro da área recortada...')
-        filteredCurvas = processing.run("native:extractbylocation", {
-            'INPUT': curvaNivel,
-            'PREDICATE': [0],  # Intersect
-            'INTERSECT': moldura,
-            'OUTPUT': 'memory:'
-        }, context=context, feedback=feedback)['OUTPUT']       
+        buffer_geometry = line_geometry.buffer(tol, 20)
+        (buffer_sink, buffer_dest_id) = self.parameterAsSink(parameters, self.OUTPUT_BUFFER, context, pontos_layer.fields(), QgsWkbTypes.Polygon, pontos_layer.sourceCrs())
+        buffer_feature = QgsFeature()
+        buffer_feature.setGeometry(buffer_geometry)
+        buffer_sink.addFeature(buffer_feature, QgsFeatureSink.FastInsert)
 
-         # Recortar o MDT com a camada de moldura
-        feedback.pushInfo('Recortando o MDT com a camada de moldura...')
-        clippedRaster = processing.run("gdal:cliprasterbymasklayer", {
-            'INPUT': raster,
-            'MASK': moldura,  # Supõe-se que a camada de moldura é a mesma que a camada de curvas de nível
-            'OUTPUT': 'TEMPORARY_OUTPUT'
-        }, context=context, feedback=feedback)['OUTPUT']
+        dia1_features = {feat[primary_key]: feat for feat in dia1_layer.getFeatures()}
+        dia2_features = {feat[primary_key]: feat for feat in dia2_layer.getFeatures()}
+        modified_features = []
+        modified_features_outside_buffer = []
 
-        # Identificar curvas de nível que não têm outras dentro
-        feedback.pushInfo('Identificando curvas isoladas...')
-        isolatedCurves = []
-        for feature in filteredCurvas.getFeatures():
-            isIsolated = True
-            for otherFeature in filteredCurvas.getFeatures():
-                if feature.id() != otherFeature.id() and feature.geometry().contains(otherFeature.geometry()):
-                    isIsolated = False
-                    break
-            if isIsolated:
-                isolatedCurves.append(feature)
+        for key in dia1_features:
+            if key in dia2_features:
+                feat1 = dia1_features[key]
+                feat2 = dia2_features[key]
+                if feat1.geometry().distance(feat2.geometry()) > tol:
+                    continue
+                modified = any(feat1[field] != feat2[field] for field in feat1.fields().names() if field not in {feat1.fields().fieldName(i) for i in ignora_indexes})
+                if modified:
+                    modified_features.append(feat2)
+                    if not buffer_geometry.contains(feat2.geometry()):
+                        modified_features_outside_buffer.append(feat2)
 
-                # Adicionando feedback sobre o número de curvas isoladas encontradas
-        feedback.pushInfo(f'Número de curvas isoladas encontradas: {len(isolatedCurves)}')
+        (mod_sink, mod_dest_id) = self.parameterAsSink(parameters, self.OUTPUT_MODIFICADOS, context, dia1_layer.fields(), dia1_layer.wkbType(), dia1_layer.sourceCrs())
+        for feat in modified_features:
+            mod_sink.addFeature(feat, QgsFeatureSink.FastInsert)
 
-        # Processar cada curva isolada para encontrar o ponto mais elevado
-        feedback.pushInfo('Processando cada curva isolada para encontrar o ponto mais elevado...')
-        maxPoints = []
-        for curve in isolatedCurves:
-            # Criar uma camada temporária para a máscara
-            maskLayer = QgsVectorLayer("Polygon?crs=epsg:4326", "temporary_mask", "memory")
-            prov = maskLayer.dataProvider()
-            feat = QgsFeature()
-            feat.setGeometry(curve.geometry())
-            prov.addFeature(feat)
-            QgsProject.instance().addMapLayer(maskLayer)  # Adicionar ao projeto se necessário para debug
+        (mod_out_sink, mod_out_dest_id) = self.parameterAsSink(parameters, self.OUTPUT_MODIFICADOS_FORA, context, dia1_layer.fields(), dia1_layer.wkbType(), dia1_layer.sourceCrs())
+        for feat in modified_features_outside_buffer:
+            mod_out_sink.addFeature(feat, QgsFeatureSink.FastInsert)
 
-            # Usar a camada temporária como máscara
-            curveRaster = processing.run("gdal:cliprasterbymasklayer", {
-                'INPUT': clippedRaster,
-                'MASK': maskLayer,
-                'OUTPUT': 'TEMPORARY_OUTPUT'
-            }, context=context, feedback=feedback)['OUTPUT']
-
-            # Converter pixels do raster em pontos
-            pointsLayer = processing.run("qgis:rasterpixelstopoints", {
-                'INPUT': curveRaster,
-                'OUTPUT': 'TEMPORARY_OUTPUT'
-            }, context=context, feedback=feedback)['OUTPUT']
-
-            # Encontrar o ponto de maior elevação
-            maxElevation = float('-inf')
-            maxPoint = None
-            for point in pointsLayer.getFeatures():
-                if point['rvalue'] > maxElevation:
-                    maxElevation = point['rvalue']
-                    maxPoint = point.geometry()
-
-            if maxPoint:
-                maxPoints.append((maxPoint, maxElevation))
-
-            QgsProject.instance().removeMapLayer(maskLayer)  # Remover a camada temporária após uso
-
-        # Criar a camada de saída com os pontos de maior elevação
-        (sinkpontoCota, sinkpontoCotaId) = self.parameterAsSink(parameters, self.OUTPUT_PONTO_COTA, context, QgsFields(), QgsWkbTypes.Point, raster.crs())
-        for point, elevation in maxPoints:
-            feat = QgsFeature()
-            feat.setGeometry(point)
-            feat.setAttributes([elevation])
-            sinkpontoCota.addFeature(feat, QgsFeatureSink.FastInsert)
-
-
-        # Garantir que todos os resultados dos processamentos sejam incluídos no dicionário de retorno
         return {
-            self.OUTPUT_PONTO_COTA: sinkpontoCotaId,
+            self.OUTPUT_CURVA: line_dest_id,
+            self.OUTPUT_MODIFICADOS: mod_dest_id,
+            self.OUTPUT_BUFFER: buffer_dest_id,
+            self.OUTPUT_MODIFICADOS_FORA: mod_out_dest_id
         }
 
+def register_algorithms():
+    QgsApplication.processingRegistry().addAlgorithm(Projeto3Solucao())
